dfdc4f779f7a589a703a07dc1a68d245471f2fdb
Goran Ninkovic <goran@goranninkovic.com>
2025-07-09 17:04:46 +0200

    chore: upgrade code quality tools and add typescript

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,17 @@
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "toggle-hide-products") {
-
+  if (command === 'toggle-hide-products') {
     chrome.storage.sync.get('hideNonLowFodmap', (data) => {
       const newState = !data.hideNonLowFodmap;
 
       chrome.storage.sync.set({ hideNonLowFodmap: newState });
 
-
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0]) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "toggleHide", hide: newState });
+        const tab = tabs[0];
+        if (tab?.id) {
+          chrome.tabs.sendMessage(tab.id, {
+            action: 'toggleHide',
+            hide: newState,
+          });
         }
       });
     });
--- src/popup.ts
+++ src/popup.ts
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleSwitch = document.getElementById('toggleSwitch');
 
-
   chrome.storage.sync.get('hideNonLowFodmap', (data) => {
     toggleSwitch.checked = !!data.hideNonLowFodmap;
   });
-
 
   toggleSwitch.addEventListener('change', () => {
     const shouldHide = toggleSwitch.checked;
     chrome.storage.sync.set({ hideNonLowFodmap: shouldHide });
 
-
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "toggleHide", hide: shouldHide });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: 'toggleHide',
+        hide: shouldHide,
+      });
     });
   });
 });
